41969a4cc973b3b139afe76f5df562aab5f305e8
Pol92 <plb92@hotmail.com>
2022-12-21 14:23:49 +0100

    - (ongoing) Creating an example to build a DLL using C++

---
+++ DLL/call_example_dll.cpp
@@ -0,0 +1,14 @@
+
+#include "example_dll.h"
+
+int main(void)
+{
+        hello("World");
+        printf("%d\n", Double(333));
+        CppFunc();
+
+        MyClass a;
+        a.func();
+
+        return 0;
+}
